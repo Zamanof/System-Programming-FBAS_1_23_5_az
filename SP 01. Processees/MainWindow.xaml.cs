acca725a890ce3016efad996c26f5f3e30f2b54b
Nadir Zamanov <Zamanov@itstep.edu.az>
2024-08-20 12:23:27 +0400

    SP 01

--- SP 01. Processees/MainWindow.xaml.cs
+++ SP 01. Processees/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace SP_01._Processees
 {
@@ -19,6 +12,31 @@ namespace SP_01._Processees
         public MainWindow()
         {
             InitializeComponent();
+            //Process.Start("cmd");
+            //Process.Start("calc");
+            //Process.Start("mspaint");
+            //Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe");
+
+            //procTextBox.Text = Process.GetCurrentProcess().ProcessName;
+            //procTextBox.Text = Process.GetCurrentProcess().Id.ToString();
+            //var process = Process.GetProcessById(7612);
+            //process.Kill();
+
+            //var processes = Process.GetProcessesByName("CalculatorApp");
+            ////processes[0].Kill();
+            //foreach (var process in processes)
+            //{
+            //    process.Kill();
+            //}
+
+            //var proc = Process.GetProcesses().ToList();
+            //foreach(var p  in proc)
+            //{
+            //    procListBox.Items.Add($"{p.Id} {p.ProcessName}");
+            //}
+
+            //ProcessStartInfo startInfo = new ProcessStartInfo("calc");
+            //Process.Start(startInfo);
         }
     }
 }
