ec52b20a2ce1e617ef2fd4ba67392872aba0a1c6
Nadir Zamanov <Zamanov@itstep.edu.az>
2024-08-20 10:59:40 +0400

    Add project files.

---
+++ SP 01. Processees/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SP_01._Processees
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
